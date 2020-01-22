21f91e819bf6bb9fb8c43cf7682369d39e159011
sakiyamayurie <yuri01301208@gmail.com>
2020-01-22 14:52:27 +0000

    showアクション

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   root 'static_pages#top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
