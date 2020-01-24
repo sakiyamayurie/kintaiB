2153c48bd6653099d2101499a7f5e9e5715fe397
sakiyamayurie <yuri01301208@gmail.com>
2020-01-24 15:39:36 +0000

    6.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
 
+  get '/signup', to: 'users#new'
   root 'static_pages#top'
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy' 
+  
   resources :users
 end
