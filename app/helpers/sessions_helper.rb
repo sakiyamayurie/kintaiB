94291160726d0039c1f7396920394024cec65fb7
sakiyamayurie <yuri01301208@gmail.com>
2020-01-27 14:19:23 +0000

    7.1.1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # セッションと@current_userを破棄します。
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
