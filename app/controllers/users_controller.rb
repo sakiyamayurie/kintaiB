a0f59839e5609c8a69ea5796f52046cb19a87f01
sakiyamayurie <yuri01301208@gmail.com>
2020-02-24 14:44:35 +0000

    header完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
   end
   
   def basic_info_params
-    params.require(:user).permit(:department, :basic_time, :work_time)
+    params.require(:user).permit(:basic_time, :work_time)
   end
 end
