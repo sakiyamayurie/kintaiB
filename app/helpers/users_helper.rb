c34f1805fb69ad37ce0b12e641dc5da03154e9af
sakiyamayurie <yuri01301208@gmail.com>
2020-03-04 14:32:24 +0000

    day

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
   def format_basic_info(time)
     format("%.2f", ((time.hour * 60) + time.min) / 60.0)
   end 
+  
+  def test(day)
+    if day.worked_on.wday == 0
+      "text-danger"
+    elsif day.worked_on.wday == 6
+      "text-info"
+    end
+  end
 end
