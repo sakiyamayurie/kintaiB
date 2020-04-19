1b9659072e83c0df637729a22abcbe920d6e3d44
sakiyamayurie <yuri01301208@gmail.com>
2020-04-19 14:23:20 +0000

    format

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -15,4 +15,13 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  def format_hour(time)
+    format("%.2d",(time.hour))
+  end
+  
+  def format_min(time)
+    format("%.2d",(((time.min) / 15) * 15))
+  end
+  
 end
